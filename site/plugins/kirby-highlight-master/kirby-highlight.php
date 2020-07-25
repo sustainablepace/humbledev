800b1d89353a53e04330a31ed4d5774fac576b05
christoph.baudson <christoph.baudson@rewe-digital.com>
2020-07-25 15:42:36 +0200

    code highlighting

---
+++ site/plugins/kirby-highlight-master/kirby-highlight.php
@@ -0,0 +1,20 @@
+<?php
+
+/**
+ * Kirby Highlight - Themeable server-side syntax highlighting for Kirby
+ *
+ * @package   Kirby CMS
+ * @author    S1SYPHOS <hello@twobrain.io>
+ * @link      http://twobrain.io
+ * @version   0.6.0
+ * @license   MIT
+ */
+
+if (c::get('plugin.kirby-highlight', false)) {
+    // Initialising composer's autoloader
+    require_once  __DIR__ . DS . 'vendor' . DS . 'autoload.php';
+
+    // Loading settings & core
+    require_once __DIR__ . DS . 'core' . DS . 'syntax_highlight.php';
+}
+
